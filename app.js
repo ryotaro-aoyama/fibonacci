80c857e272176a96965506e79572765c2a256946
ryotaro-aoyama <aoyama.a55@gmail.com>
2016-12-22 17:19:29 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 'use strict';
+const memo = new Map();
+//0番目と1番目の答えはあらかじめ定義されている。
+memo.set(0,0);
+memo.set(1,1);
+
 function fib(n){
-    if(n===0){
-        return 0;
-
-    }else if(n===1){
-        return 1;
+    if(memo.has(n)){
+        return memo.get(n);
     }
-
-    return fib(n-1) + fib(n-2);
+    const value = fib(n-1) + fib(n-2);
+    memo.set(n,value);
+    return value;
 }
+
+
+//function fib(n){
+//    if(n===0){
+//        return 0;
+//
+//    }else if(n===1){
+//        return 1;
+//    }
+//
+//    return fib(n-1) + fib(n-2);
+//}
 
 const length =40;
 for(let i = 0 ; i <= length ; i++){
